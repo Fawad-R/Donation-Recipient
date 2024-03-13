e79194f06680d233c10481248653ea395619b8fc
Fawad-R <fawadrahman55@gmail.com>
2024-03-13 17:56:48 +0500

    Commit message describing the changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Project2:M0YvGlNHYrXssfAI@cluster0.rrftaqb.mongodb.net/?retryWrites=true&w=majority');
-
+// mongoose.connect('mongodb+srv://Project2:M0YvGlNHYrXssfAI@cluster0.rrftaqb.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://fawad:VFtMEIzMZEla6m1h@cluster0.txpxugl.mongodb.net/');
+// VFtMEIzMZEla6m1h
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
